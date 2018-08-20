b5cb41807b2bf5af93ccff3b2c8e7ff5549989e5
Süha <34501436+programmer-666@users.noreply.github.com>
2018-08-21 01:35:53 +0300

    Add files via upload

--- Python/KarakterDizileri.py
+++ Python/KarakterDizileri.py
@@ -24,6 +24,56 @@ for isim in site1, site2, site3, site4:
 print(py[:4],py[2:])
 """Karakter Dizilerini Ters Çevirmek"""
 print(site[::-1])
-print(site[:15:-1], site[::2])#İkişe İkişer atlar
+print(site[:15:-1], site[::2])#İkişer İkişer atlar
 print(*reversed(site),sep="")#reversed ile yapılabilir
-#Karakter Dizilerini Alfabe Sırasına Dizmek
+#for i in reversed("Sana Gül Bahçesi Vadetmedim"): print(i, end="") - Bir çözüm
+"""Karakter Dizilerini Alfabe Sırasına Dizmek"""
+print(sorted(py))
+for h in sorted(py):
+    print(h,end="")
+#Dikkat! sorted fonksiyonun'da Türkçe karakter sorunu vardır
+#Olası çözüm için
+import locale 
+locale.setlocale(locale.LC_ALL, "Turkish_Turkey.1254") #Windows için 
+locale.setlocale(locale.LC_ALL, "tr_TR") #GNU/Linux için 
+print(sorted("çiçek", key=locale.strxfrm))
+"""KarakterDizileriÜzerindeDeğişiklikYapmak"""
+print("P" + py[1:])
+py2 = "P" + py[1:]
+print(py2,py)
+#Örnek program
+sesli_harfler = "aeıioöuü" 
+sessiz_harfler = "bcçdfgğhjklmnprsştvyz"
+sesliler = "" 
+sessizler = ""
+kelime = "istanbul"
+for i in kelime: 
+    if i in sesli_harfler: 
+        sesliler += i 
+    else: 
+        sessizler += i
+print("sesli harfler: ", sesliler) 
+print("sessiz harfler: ", sessizler)
+"""ÜçÖnemliFonksiyon"""
+#Dir()
+"""İlk olarak dir() adlı özel bir fonksiyondan söz edeceğiz. Bu metot bize Python’daki bir
+nesnenin özellikleri hakkında bilgi edinme imkanı verecek. Mesela karakter dizilerinin bize
+hangi metotları sunduğunu görmek için bu fonksiyonu kullanabiliriz."""
+print(dir(x),"---------------------\n")
+print(dir(""))
+for j in dir(""):
+    if "_" != j[0]:
+        print(j)
+#enumerate()
+"""Eğer yazdığınız bir programda numaralandırmaya ilişkin işlemler yapmanız gerekiyorsa
+Python’ın size sunduğu çok özel bir fonksiyondan yararlanabilirsiniz. Bu fonksiyonun adı
+enumerate()."""
+print(enumerate(py))
+print(*enumerate(py))
+for sira, metot in enumerate(dir("")):
+    print(sira, metot)
+#help()
+"""Python’la ilgili herhangi bir konuda yardıma ihtiyacınız olduğunda, internetten araştırma
+yaparak pek çok ayrıntılı belgeye ulaşabilirsiniz. Ama eğer herhangi bir nesne hakkında
+hızlı bir şekilde ve İngilizce olarak yardım almak isterseniz help() adlı özel bir fonksiyondan
+yararlanabilirsiniz."""
