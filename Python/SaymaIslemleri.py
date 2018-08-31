72f2e3db17d326bd708b18aa61f98944fe6592e0
Süha <34501436+programmer-666@users.noreply.github.com>
2018-08-31 23:50:56 +0300

    Add files via upload

---
+++ Python/SaymaIslemleri.py
@@ -0,0 +1,24 @@
+pi = 3.14
+x = 1234
+#OnluSaymaSistemi - 0, 1, 2, 3, 4, 5, 6, 7, 8, 9
+#Sekizli - 0, 1, 2, 3, 4, 5, 6, 7
+#OnAltili - 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, a, b, c, d, e, f
+#Ikili - 0, 1
+#Sayma sistelerini birbirine donusturmu
+#FonkIle
+#bin = Binary
+print(bin(x)[2:])
+#hex - Hexadecimal
+print(hex(x)[2:])
+#oct - Octal
+print(oct(x)[2:])
+#int - Integeer
+print(int(x))
+#Bicimlendirme Yoluyla
+#Binary
+print('{:b}'.format(x))
+#Hexa
+print('{:x}'.format(x))
+#Octa
+print('{:o}'.format(x))
+#Ozetlendi...
